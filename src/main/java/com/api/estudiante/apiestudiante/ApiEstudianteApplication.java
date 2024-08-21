efb81547f097474baaea4d204485abe0ccc23f8e
dbastomo@emeal.nttdata.com <dbastomo@emeal.nttdata.com>
2024-08-21 15:17:55 -0500

    Primer commit app

---
+++ src/main/java/com/api/estudiante/apiestudiante/ApiEstudianteApplication.java
@@ -0,0 +1,13 @@
+package com.api.estudiante.apiestudiante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEstudianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEstudianteApplication.class, args);
+	}
+
+}
